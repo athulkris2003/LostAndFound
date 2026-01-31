5390e0275513a784888a9c583b67d8c1d2fae4aa
Athul krishna <athulkrishna2k3@gmail.com>
2026-01-31 10:23:30 +0530

    database changed to postgresql

--- lostandfound/settings.py
+++ lostandfound/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "lostandfound.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "lostandfoundDB",
+        "USER": "postgres",
+        "PASSWORD": "Athul@2003",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
